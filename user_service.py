c0a3a4282b9b3e80132a7387bd69469d19358570
Khairul Slt <khairulslt@hotmail.sg>
2019-04-01 22:20:10 +0800

    update user_service.py: define settings, make_app

--- user_service.py
+++ user_service.py
@@ -29,9 +29,43 @@ class App(tornado.web.Application):
             + ");"
         )
         self.db.commit()
-        
+
 class BaseHandler(tornado.web.RequestHandler):
     def write_json(self, obj, status_code=200):
         self.set_header("Content-Type", "application/json")
         self.set_status(status_code)
         self.write(json.dumps((obj), indent=4))
+
+# /users/ping
+class PingHandler(tornado.web.RequestHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        self.write("pong!")
+
+def make_app(options):
+    return App([
+        (r"/users/ping", PingHandler),
+        (r"/users", UsersHandler),
+    ], debug=options.debug)
+
+if __name__ == "__main__":
+    # Define settings/options for the web app
+    # Specify the port number to start the web app on (default value is port 6000)
+    tornado.options.define("port", default=6000)
+    # Specify whether the app should run in debug mode
+    # Debug mode restarts the app automatically on file changes
+    tornado.options.define("debug", default=True)
+
+    # Read settings/options from command line
+    tornado.options.parse_command_line()
+
+    # Access the settings defined
+    options = tornado.options.options
+
+    # Create web app
+    app = make_app(options)
+    app.listen(options.port)
+    logging.info("Starting listing service. PORT: {}, DEBUG: {}".format(options.port, options.debug))
+
+    # Start event loop
+    tornado.ioloop.IOLoop.instance().start()
