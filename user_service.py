a5d0d41d685f9793e13b97bccfbb43c772f3b824
Khairul Slt <khairulslt@hotmail.sg>
2019-04-01 23:36:21 +0800

    update user_service.py: add GET and POST handlers for /users endpoint

--- user_service.py
+++ user_service.py
@@ -30,6 +30,96 @@ class App(tornado.web.Application):
         )
         self.db.commit()
 
+# /users
+class UsersHandler(BaseHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        # Parsing pagination params
+        page_num = self.get_argument("page_num", 1)
+        page_size = self.get_argument("page_size", 10)
+        try:
+            page_num = int(page_num)
+        except:
+            logging.exception("Error while parsing page_num: {}".format(page_num))
+            self.write_json({"result": False, "errors": "invalid page_num"}, status_code=400)
+            return
+
+        try:
+            page_size = int(page_size)
+        except:
+            logging.exception("Error while parsing page_size: {}".format(page_size))
+            self.write_json({"result": False, "errors": "invalid page_size"}, status_code=400)
+            return
+
+        # Building select statement
+        select_stmt = "SELECT * FROM users"
+        # Order by and pagination
+        limit = page_size
+        offset = (page_num - 1) * page_size
+        select_stmt += " ORDER BY created_at DESC LIMIT ? OFFSET ?"
+
+        # Fetching users from db
+        args = (limit, offset)  
+        cursor = self.application.db.cursor()
+        results = cursor.execute(select_stmt, args)
+
+        users = []
+        for row in results:
+            fields = ["id", "name", "created_at", "updated_at"]
+            user = {
+                field: row[field] for field in fields
+            }
+            users.append(user)
+
+        self.write_json({"result": True, "users": users})
+
+    @tornado.gen.coroutine
+    def post(self):
+        # Collecting required params
+        name = self.get_argument("name")
+        # Validating inputs
+        errors = []
+        name_val = self._validate_name(name, errors)
+        time_now = int(time.time() * 1e6) # Converting current time to microseconds
+
+        # End if we have any validation errors
+        if len(errors) > 0:
+            self.write_json({"result": False, "errors": errors}, status_code=400)
+            return
+            
+        # Proceed to store the users in our db
+        cursor = self.application.db.cursor()
+        cursor.execute(
+            "INSERT INTO 'users' "
+            + "('name', 'created_at', 'updated_at') "
+            + "VALUES (?, ?, ?)",
+            (name_val, time_now, time_now)
+        )
+        self.application.db.commit()
+
+        # Error out if we fail to retrieve the newly created user
+        if cursor.lastrowid is None:
+            self.write_json({"result": False, "errors": ["Error while adding user to db"]}, status_code=500)
+            return
+
+        user = dict(
+            id=cursor.lastrowid,
+            name=name_val,
+            created_at=time_now,
+            updated_at=time_now
+        )
+
+        self.write_json({"result": True, "user": user})
+
+    def _validate_name(self, name, errors):
+        try:
+            name = str(name)
+            return name
+        except Exception as e:
+            logging.exception("Error while converting name to str: {}".format(name))
+            errors.append("invalid name")
+            return None
+            
 class BaseHandler(tornado.web.RequestHandler):
     def write_json(self, obj, status_code=200):
         self.set_header("Content-Type", "application/json")
